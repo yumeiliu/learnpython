66e8b13c5d64c4a5ec6a0ebe7fc0a032dca22cdb
yumeiliu <1751834043@qq.com>
2017-03-18 22:34:28 +0800

    practice input & print usrname

---
+++ input.py
@@ -0,0 +1,5 @@
+name = input()
+print('hello', name)
+
+#use print() print user name
+
---
+++ print.py
@@ -0,0 +1,3 @@
+print("The quick brown fox", "jumps over", "the lazy dog")
+name = input()
+print('hello', name)
